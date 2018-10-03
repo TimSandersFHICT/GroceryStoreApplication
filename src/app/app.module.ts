8e27c9a6e9a342e3fb4637e6432e6e34c7a11686
Tim S <tim.sanders@student.fontys.nl>
2018-10-03 12:50:31 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GroceryListComponent } from './core/groceries/grocery-list.component';
+import { AddFormComponent } from './core/form/add-form.component';
+import { MainComponent } from './core/main.component';
+import { SortablejsModule } from 'angular-sortablejs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryListComponent,
+    AddFormComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SortablejsModule.forRoot({ animation: 150 })
   ],
   providers: [],
   bootstrap: [AppComponent]
